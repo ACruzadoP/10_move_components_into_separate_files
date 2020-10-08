d73e4edb0da5bd7ed786a2188497e2dd32ddaa04
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:53:35 +0200

    Tomando snapshot de un ejercicio terminado

---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Alberto</h1>
+        <p>This is a paragraph about me</p>
+        <ul>
+            <li>Chile</li>
+            <li>Greece</li>
+            <li>Thailand</li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default MyInfo
+  
